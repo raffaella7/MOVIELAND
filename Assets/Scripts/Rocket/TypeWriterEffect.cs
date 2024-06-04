7f6b06c3320340300171735cef24c2218b68953f
raffaella7 <raffaellareccini@gmail.com>
2024-06-04 14:45:22 +0200

    add razzo

---
+++ Assets/Scripts/Rocket/TypeWriterEffect.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TypewriterEffect : MonoBehaviour
+{
+    public Text uiText;  // rif al Text dell'UI
+    public float delay = 0.1f;  // delay tra una lettera e l'altra
+
+    private string fullText;  // testo completo che vediamo
+    private string currentText = "";  // testo che sto vedendo adesso
+
+    void Start()
+    {
+        fullText = uiText.text; //testo completo
+       
+        StartCoroutine(ShowText()); //inizio la corutine per l'effetto
+    }
+
+    IEnumerator ShowText()
+    {
+        
+        for (int i = 0; i <= fullText.Length; i++)  //ciclo dalla prima lettera fino alla lunghezza del testo completo
+        {
+            currentText = fullText.Substring(0, i); //estraggo una sottostringa del testo completo, ovvero da 0 all'indice i
+            
+            uiText.text = currentText;  //aggiorno il testo visualizzato con la sottostringa di quel moemnto
+            
+            yield return new WaitForSeconds(delay);     //aspetto per il delay prima di vedere la prossima lettera
+        }
+    }
+           
+
+}
