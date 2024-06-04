9299939b822436fa8f5f186fa39760dce33ae8b3
David <davidbork7@gmail.com>
2024-06-04 17:05:52 +0200

    stuff

---
+++ Assets/_David/RocketSplitParts.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketSplitParts : MonoBehaviour
+{
+    void Start()
+    {
+        //Move apart each children(part) of the rocket
+
+    }
+
+}
