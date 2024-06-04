d44c9bda12374ad6ea69b614977251c6b74683dc
David <davidbork7@gmail.com>
2024-06-04 17:47:06 +0200

    feat: Add rocket split functionality

--- Assets/_David/RocketSplitParts.cs
+++ Assets/_David/RocketSplitParts.cs
@@ -4,10 +4,23 @@ using UnityEngine;
 
 public class RocketSplitParts : MonoBehaviour
 {
+    Vector3[] newPositions;
+    [SerializeField] GameObject SplitRocket;
+
+    void Awake()
+    {
+        newPositions = new Vector3[SplitRocket.transform.childCount];
+        for (int i = 0; i < SplitRocket.transform.childCount; i++)
+        {
+            newPositions[i] = SplitRocket.transform.GetChild(i).transform.localPosition;
+            print(newPositions[i] + " " + SplitRocket.transform.GetChild(i).name);
+        }
+        SplitRocket.SetActive(false);
+    }
+
     void Start()
     {
-        foreach (Transform child in transform)
-            print(child.name);
+
 
     }
 
