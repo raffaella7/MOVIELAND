4cd8f4e0753d3e69f6d2f0420a367386fafab646
raffaella7 <raffaellareccini@gmail.com>
2024-06-05 12:16:46 +0200

    add animation to planets, add text to canvas

---
+++ Assets/Scripts/Rocket/PlanetsRotation.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetsRotation : MonoBehaviour
+{
+    float speed = 0.5f;
+    void Update()
+    {
+        transform.RotateAround(Vector3.zero, Vector3.up, speed * Time.deltaTime);
+        transform.LookAt(Vector3.zero);
+    }
+}
