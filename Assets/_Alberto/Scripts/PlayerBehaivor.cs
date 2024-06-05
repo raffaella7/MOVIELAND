6d25c2f155c97f0c3bf7ed96ab297ee45c86a7cb
albertobott <albertobotter.01@gmail.com>
2024-06-05 15:45:29 +0200

    ADD- scripts
    PlayerBehaivour-InputManager

---
+++ Assets/_Alberto/Scripts/PlayerBehaivor.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaivor : MonoBehaviour
+{
+    private GameObject player;
+    private int currentLane; 
+    void Awake()
+    {
+       player= FindAnyObjectByType<GameObject>(); 
+    }
+
+    public void Left()
+    {
+        currentLane--;
+        currentLane = Mathf.Clamp(currentLane,0,2);
+    }
+    public void Right()
+    {
+        currentLane++;
+        currentLane = Mathf.Clamp(currentLane,0,2);
+    }
+    
+}
