4aad9606c2ca04b5082f8d0f789c9c5df593724a
raffaella7 <raffaellareccini@gmail.com>
2024-06-07 13:27:40 +0200

    add- truck

---
+++ Assets/_Alberto/Scripts/TruckBehaviour.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Rendering;
+using UnityEngine;
+
+public class TruckBehaviour : MonoBehaviour
+{
+    GameManager gameManager;
+    private float speed = 15f;
+
+    void Awake()
+    {
+        gameManager = FindAnyObjectByType<GameManager>();
+    }
+    void Update()
+    {
+        MoveObject();
+    }
+    void MoveObject()
+    {
+        // Usa la variabile 'speed' per la velocità
+        transform.Translate(gameManager.MovementDirection * Time.deltaTime * speed);
+    }
+}
